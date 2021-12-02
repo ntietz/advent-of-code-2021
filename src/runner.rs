f5b5d7434beb00421ec4ec8d53d051eac82509b2
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-12-02 08:03:33 -0500

    Complete day 2

--- src/runner.rs
+++ src/runner.rs
@@ -1,8 +1,7 @@
-use crate::day01;
-
 pub fn run(day: u32) {
     match day {
-        1 => day01::run(),
+        1 => crate::day01::run(),
+        2 => crate::day02::run(),
         _ => panic!("Unfinished day"),
     }
 }
