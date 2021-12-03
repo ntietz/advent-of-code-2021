7e8c76a7cbbfc08c19fc46ed1c8a1954a9e2a35b
Nicholas Tietz-Sokolsky <me@ntietz.com>
2021-12-03 08:42:00 -0500

    Add solution to day 3, both parts

--- src/runner.rs
+++ src/runner.rs
@@ -2,6 +2,7 @@ pub fn run(day: u32) {
     match day {
         1 => crate::day01::run(),
         2 => crate::day02::run(),
+        3 => crate::day03::run(),
         _ => panic!("Unfinished day"),
     }
 }
