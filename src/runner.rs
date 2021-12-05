42d086cb24bfc6e08c9fcb84236e9bcc92c0d10a
Nicole Tietz-Sokolskaya <me@ntietz.com>
2021-12-05 10:39:39 -0500

    Complete day 5

--- src/runner.rs
+++ src/runner.rs
@@ -4,6 +4,7 @@ pub fn run(day: u32) {
         2 => crate::day02::run(),
         3 => crate::day03::run(),
         4 => crate::day04::run(),
+        5 => crate::day05::run(),
         _ => panic!("Unfinished day"),
     }
 }
